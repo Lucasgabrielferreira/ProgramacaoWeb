543a296faa4dfc7bb9e0fd332ec30220d189527e
Lucas Gabriel <lg139826065@gmail.com>
2023-10-17 21:45:36 -0300

    Aulas JavaScript

---
+++ JavaScript/Atividade04/lacosRepeticao.js
@@ -0,0 +1,26 @@
+//Mostrar no console os números de 1 ao 50. (while)
+
+let cont = 1;
+while (cont <= 50) {
+    console.log(`Numero ${cont}`);
+    cont++;
+}
+
+
+//Mostrar no console contagem regressiva de 10 até 0.(do-while)
+
+console.clear();
+let l = 10, text = '';
+do {
+    text += `Contagem Regressiva = ${l}\n`;
+    l--;
+} while (l > 0);
+console.log(text);
+
+
+//Mostrar no console os números pares de 1 a 100. (for)
+console.clear();
+for (let i = 0; i < 100; i++){
+    Pares = i / 2;
+} 
+console.log(`Números pares: ${i}`);
