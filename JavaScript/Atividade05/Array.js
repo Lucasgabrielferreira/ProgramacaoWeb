6f7da11e6cbc62d924156dab3f8712450323a57f
Lucas Gabriel <lg139826065@gmail.com>
2023-10-18 21:51:33 -0300

    Aulas JavaScript

---
+++ JavaScript/Atividade05/Array.js
@@ -0,0 +1,13 @@
+let times = ["Palmeiras", "Santos", "Portuguesa", "Ponte Preta", "Corithians"]
+
+times.pop()
+
+console.log(times);
+
+times.shift()
+
+console.log(times);
+
+times.push("São Paulo");
+
+times.unshift("Flamengo");
