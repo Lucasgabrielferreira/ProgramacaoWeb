e45ec3340511302f10637345d49114506b5c8bac
Lucas Gabriel <lg139826065@gmail.com>
2023-10-24 21:45:20 -0300

    Aulas JavaScript

---
+++ JavaScript/Atividade08/metodos.js
@@ -0,0 +1,19 @@
+let funcionarios = [{
+    id: 1,
+    nome: "Lucas",
+    salario: 5.000,
+}, 
+{
+    id: 2,
+    nome: "Wally",
+    salario: 10.000,
+},
+{
+    id: 3,
+    nome: "Gabriel",
+    salario: 4.000,
+}
+];
+funcionarios.forEachforEach(() => {
+    
+});
