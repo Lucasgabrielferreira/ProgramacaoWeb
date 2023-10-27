cb23c50cb7e1bb48f89b9e2a65b5af7cdf83d9cb
Lucas Gabriel <lg139826065@gmail.com>
2023-10-26 21:40:41 -0300

    Aulas JavaScript

--- JavaScript/Atividade08/metodos.js
+++ JavaScript/Atividade08/metodos.js
@@ -1,18 +1,39 @@
-let funcionarios = [{
-    id: 1,
-    nome: "Lucas",
-    salario: 5.000,
-}, 
-{
-    id: 2,
-    nome: "Wally",
-    salario: 10.000,
-},
-{
-    id: 3,
-    nome: "Gabriel",
-    salario: 4.000,
-}
+let funcionarios = [
+    {
+        id: 1,
+        nome: 'Lucas',
+        salario: 8000,
+    },
+    {
+        id: 2,
+        nome: 'Wally',
+        salario: 4800,
+    },
+    {
+        id: 3,
+        nome: 'Gabriel',
+        salario: 1000,
+    },
+    {
+        id: 4,
+        nome: 'Ferreira',
+        salario: 3000,
+    },
+    {
+        id: 5,
+        nome: 'Victor',
+        salario: 5000,
+    },
 ];
 
-funcionarios.forEach((Nome) => console.log(Nome.nome));
+funcionarios.forEach((funcionarioss) => {
+    console.log(funcionarioss.nome);
+    console.log(`Salario: ${funcionarioss.salario}`);
+});
+
+
+let reajuste = funcionarios.map((funcionarioss) => {
+
+    return funcionarioss.salario * 1.05;
+});
+console.log(`Novos salarios após reajuste de 5% ${reajuste}`);
