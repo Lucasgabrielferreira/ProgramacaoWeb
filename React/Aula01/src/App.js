a53469f3ea83c903911b68d1b5411574a6d89adb
Lucas Gabriel <lg139826065@gmail.com>
2023-11-16 20:22:49 -0300

    Aulas React

---
+++ React/Aula01/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Olá, Mundo</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Lucasgabrielferreira"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lucas Gabriel Ferreira
+          
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
