3d9581d55ed42300c36594f677bafae73e2cc33c
Lucas Gabriel <lg139826065@gmail.com>
2023-11-23 21:40:23 -0300

    Aulas React

--- React/atividade02/src/Components/Conteudo.jsx
+++ React/atividade02/src/Components/Conteudo.jsx
@@ -1,13 +1,10 @@
 import { Component } from "react";
+import ButtonStyle from "./ButtonStyle";
 
 const Botao = {
     margin: "10Px",  
     fontSize: "12px",
   };
-
-//   constructor(Btn1) = {
-//       color: "green",
-//   }
 
 export class Conteudo extends Component {
   render() {
@@ -16,7 +13,7 @@ export class Conteudo extends Component {
         <article>
           <h2>Ferreira</h2>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus maiores quae harum aperiam, commodi nesciunt, ratione aut est totam facilis et, aliquid maxime eius nemo nihil distinctio nobis? Suscipit, minima.</p>
-          <button style={Botao} onClick={}>Lido</button>
+          <button style={Botao} onClick={ButtonStyle}>Lido</button>
           <button style={Botao}>Gostei</button>
           <button style={Botao}>Não Gostei</button>
         </article>
