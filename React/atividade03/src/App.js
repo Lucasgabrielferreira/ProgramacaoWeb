d40119ad9144199d2875fce5740b4215e6347081
Lucas Gabriel <lg139826065@gmail.com>
2023-12-01 17:46:46 -0300

    Aulas React

--- React/atividade03/src/App.js
+++ React/atividade03/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import List from './Components/List';
+import ListItem from './Components/ListItem';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Aplicativo de Lista</h1>
+       <List />
+       <ListItem />
     </div>
   );
 }
